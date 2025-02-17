5187a078e2d50c1d3a0d9efd7fbf5b44284b58f2
Manuel Rocholz <manuel.rocholz@voss.net>
2025-02-17 16:36:57 +0100

    Implemented FeedSchedule into Horse Profile

--- src/components/Profiles/HorseProfile.tsx
+++ src/components/Profiles/HorseProfile.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
+import { AutoAwesome, WidthFull } from "@mui/icons-material";
 import { Horse } from "../../context/AppContextProvider";
 import ProfileDialog from "../common/ProfileDialog";
+import {
+  Paper,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
-const HorseProfile: React.FC<{ horse: Horse | null; onClose: () => void }> = ({ horse, onClose}) => {
-    if (!horse) return null;
+const HorseProfile: React.FC<{ horse: Horse | null; onClose: () => void }> = ({
+  horse,
+  onClose,
+}) => {
+  if (!horse) return null;
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.secondary.main,
+    },
+    "&:nth-of-type(even)": {
+      backgroundColor: theme.palette.background.default,
+    },
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
 
   return (
     <ProfileDialog
@@ -11,30 +46,46 @@ const HorseProfile: React.FC<{ horse: Horse | null; onClose: () => void }> = ({ 
       onClose={onClose}
       title={`${horse.name}'s Profil`}
     >
-        <>
-          <img
-            src={"sample-horse-avatar.webp"}
-            alt={`${horse.name}'s avatar`}
-            width="100px"
-            height="100px"
-          />
-          <p>
-            <strong>Standort:</strong> {horse.stall.stallLocation.name}
-          </p>
-          <p>
-            <strong>Box:</strong> {horse.stall.stallNumber}
-          </p>
-          <p>
-            <strong>Besitzer:</strong> {horse.tenant.name}
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-            voluptatum impedit, ea nesciunt dolores laudantium quos? Modi fugit,
-            esse, placeat animi sed ullam magni consectetur assumenda et sint
-            neque accusantium.
-          </p>
-        </>
+      <>
+        <img
+          src={"sample-horse-avatar.webp"}
+          alt={`${horse.name}'s avatar`}
+          width="100px"
+          height="100px"
+        />
+        <p>
+          <strong>Standort:</strong> {horse.stall.stallLocation.name}
+        </p>
+        <p>
+          <strong>Box:</strong> {horse.stall.stallNumber}
+        </p>
+        <p>
+          <strong>Besitzer:</strong> {horse.tenant.name}
+        </p>
+          <strong>Futterplan:</strong>
+          <TableContainer component={Paper}>
+            <Table sx={{ tableLayout: 'fixed', width: '100%' }} aria-label="customized table">
+
+              <TableBody>
+                {horse.feedSchedule.feedings.map((feeding) => (
+                  <StyledTableRow key={feeding.id}>
+                    <StyledTableCell align="left" component="th" scope="row">
+                      <strong>{feeding.timeExpression}</strong>
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {feeding.feedType.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {feeding.feedServingSize.name}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+      </>
     </ProfileDialog>
   );
 };
-export default HorseProfile
+
+export default HorseProfile;
--- src/context/AppContextProvider.tsx
+++ src/context/AppContextProvider.tsx
@@ -19,6 +19,7 @@ export interface Horse {
   note: string;
   tenant: Tenant;
   stall: Stall;
+  feedSchedule: FeedSchedule;
 }
 
 export interface Stall {
@@ -35,6 +36,33 @@ export interface StallLocation {
   stalls: Stall[];
 }
 
+export interface FeedSchedule {
+  id: number;
+  note: String;
+  horse: Horse;
+  feedings: Feeding[];
+}
+
+export interface Feeding {
+  id: number;
+  timeExpression: String;
+  feedSchedule: FeedSchedule;
+  feedServingSize: FeedServingSize;
+  feedType: FeedType;
+}
+
+export interface FeedServingSize {
+  id: number;
+  name: String;
+  feedings: Feeding[];
+}
+
+export interface FeedType {
+  id: number;
+  name: String;
+  feedings: Feeding[];
+}
+
 interface GlobalState {
   tenants: Tenant[];
   setTenants: React.Dispatch<React.SetStateAction<Tenant[]>>;
@@ -44,6 +72,14 @@ interface GlobalState {
   setStalls: React.Dispatch<React.SetStateAction<Stall[]>>;
   stallLocations: StallLocation[];
   setStallLocations: React.Dispatch<React.SetStateAction<StallLocation[]>>;
+  feedSchedules: FeedSchedule[];
+  setFeedSchedules: React.Dispatch<React.SetStateAction<FeedSchedule[]>>;
+  feedings: Feeding[];
+  setFeedings: React.Dispatch<React.SetStateAction<Feeding[]>>;
+  feedServingSizes: FeedServingSize[];
+  setFeedServingSizes: React.Dispatch<React.SetStateAction<FeedServingSize[]>>;
+  feedTypes: FeedType[];
+  setFeedTypes: React.Dispatch<React.SetStateAction<FeedType[]>>;
 }
 
 const AppContext = createContext<GlobalState | undefined>(undefined);
@@ -63,8 +99,13 @@ export const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({
   const [horses, setHorses] = useState<Horse[]>([]);
   const [stalls, setStalls] = useState<Stall[]>([]);
   const [stallLocations, setStallLocations] = useState<StallLocation[]>([]);
+  const [feedSchedules, setFeedSchedules] = useState<FeedSchedule[]>([]);
+  const [feedings, setFeedings] = useState<Feeding[]>([]);
+  const [feedServingSizes, setFeedServingSizes] = useState<FeedServingSize[]>(
+    []
+  );
+  const [feedTypes, setFeedTypes] = useState<FeedType[]>([]);
 
-  // Optionally preload data
   useEffect(() => {
     fetch("http://localhost:80/tenants")
       .then((res) => res.json())
@@ -85,11 +126,48 @@ export const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({
       .then((res) => res.json())
       .then((data) => setStallLocations(data))
       .catch(console.error);
+
+    fetch("http://localhost:80/feed-schedules")
+      .then((res) => res.json())
+      .then((data) => setFeedSchedules(data))
+      .catch(console.error);
+
+    fetch("http://localhost:80/feedings")
+      .then((res) => res.json())
+      .then((data) => setFeedings(data))
+      .catch(console.error);
+
+      fetch("http://localhost:80/feed-serving-sizes")
+      .then((res) => res.json())
+      .then((data) => setFeedServingSizes(data))
+      .catch(console.error);
+
+      fetch("http://localhost:80/feed-types")
+      .then((res) => res.json())
+      .then((data) => setFeedTypes(data))
+      .catch(console.error);
   }, []);
 
   return (
     <AppContext.Provider
-      value={{ tenants, setTenants, horses, setHorses, stalls, setStalls, stallLocations, setStallLocations }}
+      value={{
+        tenants,
+        setTenants,
+        horses,
+        setHorses,
+        stalls,
+        setStalls,
+        stallLocations,
+        setStallLocations,
+        feedSchedules,
+        setFeedSchedules,
+        feedings,
+        setFeedings,
+        feedServingSizes,
+        setFeedServingSizes,
+        feedTypes,
+        setFeedTypes
+      }}
     >
       {children}
     </AppContext.Provider>
